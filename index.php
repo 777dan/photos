bb60640df4a61d03d58ccad42c95fe5f0774f316
777dan <danya.nikitin7@gmail.com>
2022-11-11 09:43:13 +0200

    v 1.0

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$dir = "./images";
+$arr_files = scandir($dir);
+for ($i = 0; $i < count($arr_files); $i++) {
+    if ($arr_files[$i] != "." && $arr_files[$i] != "..") {
+        $month = substr($arr_files[$i], 8, 2);
+        rename("images/".$arr_files[$i], "$month/$arr_files[$i]");
+    }
+}
