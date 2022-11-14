87d4fcd40cfb4cefaf1eba1a3f4945ccd137f3ec
777dan <danya.nikitin7@gmail.com>
2022-11-14 17:51:10 +0200

    add creating files

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $arr_files = scandir($dir);
 for ($i = 0; $i < count($arr_files); $i++) {
     if ($arr_files[$i] != "." && $arr_files[$i] != "..") {
         $month = substr($arr_files[$i], 8, 2);
+        if (!is_dir($month)){
+        mkdir($month);
+        }
         rename("images/".$arr_files[$i], "$month/$arr_files[$i]");
     }
 }
